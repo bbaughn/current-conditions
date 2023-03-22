a0b91c822d7d0616f5364a9f8b161a516f6c6c1f
Brian Baughn <developer@manufacturingcontent.com>
2023-03-22 15:34:41 -0700

    Blank xcode project

---
+++ CurrentConditions/CurrentConditionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentConditionsApp.swift
+//  CurrentConditions
+//
+//  Created by Brian Baughn on 3/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CurrentConditionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
